b32fad717ed2f637ada4985b9a22f3b2bbda9c62
Ben Tideswell <ben@fishpig.co.uk>
2018-03-23 09:12:13 +0000

    1.0.0.4

--- app/code/community/Fishpig/Wordpress/Addon/AMP/Model/Observer.php
+++ app/code/community/Fishpig/Wordpress/Addon/AMP/Model/Observer.php
@@ -4,6 +4,37 @@
 **/
 class Fishpig_Wordpress_Addon_AMP_Model_Observer
 {
+	/*
+	 *
+	 * Determine whether the current request is an AMP request
+	 * Check $_GET and $_SERVER['REQUEST_URI'] as some FPC's
+	 * Remove 'amp' from $_GET
+	 *
+	 *
+	 * @return bool
+	 *
+	 */
+	public function isAmp()
+	{
+		if (1 === (int)Mage::app()->getRequest()->getParam('amp')) {
+			return true;
+		}
+		
+		if (!empty($_SERVER['REQUEST_URI'])) {
+			$requestUri = $_SERVER['REQUEST_URI'];
+			
+			if (strpos($requestUri, '?') !== false) {
+				$queryString = ltrim(substr($requestUri, strpos($requestUri, '?')), '?');
+				
+				if (strpos($queryString, 'amp=1') !== false) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+	
 	/**
 	  *
 	  * @param Varien_Event_Observer $observer
@@ -13,7 +44,7 @@ class Fishpig_Wordpress_Addon_AMP_Model_Observer
 	 **/
 	public function wordpressPostControllerPreDispatchAfterObserver(Varien_Event_Observer $observer)
 	{
-		if (1 !== (int)Mage::app()->getRequest()->getParam('amp')) {
+		if (!$this->isAmp()) {
 			return $this;
 		}
 		
